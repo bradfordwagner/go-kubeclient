9c068dc62909119de9ff6df41999d76387b72773
bwagner <bwagner@blackrock.com>
2025-03-31 18:09:26 -0400

    configmap delete

---
+++ kube/configmap_delete.go
@@ -0,0 +1,16 @@
+package kube
+
+import (
+	"context"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ConfigmapDelete deletes a configmap
+func (c *client) ConfigmapDelete(ctx context.Context, namespace, name string) (err error) {
+	err = c.kubeClient.CoreV1().ConfigMaps(namespace).Delete(ctx, name, metav1.DeleteOptions{})
+	if !errors.IsNotFound(err) {
+		err = nil
+	}
+	return
+}
